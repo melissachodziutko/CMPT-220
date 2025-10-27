be6c6271d0989ed99e2666605cac9ea46af5622e
Melissa Chodziutko <11694558+melissachodziutko@users.noreply.github.com>
2025-10-27 09:49:26 -0400

    Create main.java

---
+++ Assignment 7/main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        //create a generic enemy and call its functions
+
+        //create a fire and an ice wizard and call all functions
+
+        //create a goblin and call its functions
+
+    }
+}
